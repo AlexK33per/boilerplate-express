37a7040a35de63ac8f599b1ab3f2c0dbeec69694
alexk33per <aalonsopersonal@gmail.com>
2022-03-09 13:36:04 +0100

    forgot next() in mware

--- myApp.js
+++ myApp.js
@@ -24,6 +24,7 @@ function sendJSON(req, res) {
 
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
 });
 app.get('/', sendExpressFile);
 app.get('/json', sendJSON);
