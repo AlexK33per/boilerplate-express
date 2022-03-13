0bbba07b16111295a5a0b8ffd1175f396ea53b37
k33per <aalonsopersonal@gmail.com>
2022-03-14 00:16:43 +0100

    added BodyParser

--- myApp.js
+++ myApp.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 require('dotenv').config();
+var bodyParser = require('body-parser');
 
 absolutePath = __dirname + '/views/index.html';
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/public", express.static(__dirname + "/public"));
 
 function sendExpressFile(req, res)  {
